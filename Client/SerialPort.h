ccef09ee61d70d7698ddcd41c88de8f7981ac60e
Oleh Terno <dzonoleg@gmail.com>
2024-11-24 13:58:17 +0200

    added tictactoe with 3 modes

--- Client/SerialPort.h
+++ Client/SerialPort.h
@@ -1,10 +1,23 @@
-#pragma once
-#include <windows.h>
-#include <iostream>
+#ifndef SERIALPORT_H
+#define SERIALPORT_H
+#include <fstream> 
 #include <string>
+#include <windows.h> // Для використання Windows API
 
-HANDLE openSerialPort(const std::wstring& portName, DWORD baudRate);
+extern std::string port;
+extern int baudRate;
 
-void sendMessage(HANDLE hSerial, const std::string& message);
+class SerialCommunication {
+private:
+    HANDLE hSerial = INVALID_HANDLE_VALUE;
 
-std::string readMessage(HANDLE hSerial);
+public:
+    bool connect(const std::string& port, int baudRate);
+    std::string sendMessage(const std::string& message);
+    void disconnect();
+    void drawBoard(const std::string& boardState);
+};
+
+void loadConfig(const std::string& filename);
+
+#endif // COMMUNICATION_H
--- Client/TikTakToe.cpp
+++ Client/TikTakToe.cpp
@@ -1,31 +1,123 @@
 #include <iostream>
 #include "SerialPort.h"
 
-int main() {
-   
-    HANDLE hSerial = openSerialPort(L"COM5", 9600);
-    if (hSerial == INVALID_HANDLE_VALUE) {
-        std::cerr << "Failed to open serial port." << std::endl;
-        return 1;
+int main()
+{
+    try
+    {
+        // Завантажуємо конфігурацію з файлу
+        loadConfig("../Config/config.json");
+        std::cout << "Configuration loaded: Port = " << port << ", BaudRate = " << baudRate << std::endl;
+
+        SerialCommunication serial;
+
+        if (!serial.connect(port, baudRate))
+        {
+            std::cerr << "Unable to connect to Arduino!" << std::endl;
+            return 1;
+        }
+
+
+        std::cout << "Welcome to the game of Tic-Tac-Toe!" << std::endl;
+        std::string response = serial.sendMessage("StartGame\n");
+
+        if (response.find("GameStarted") != std::string::npos)
+        {
+            std::cout << "The game has successfully started." << std::endl;
+
+            std::cout << "Choose game mode (1 - Man vs Man, 2 - Man vs AI, 3 - AI vs AI): ";
+            std::string mode;
+            std::getline(std::cin, mode);
+            response = serial.sendMessage("SetMode " + mode + "\n");
+            std::cout << "Server response: " << response << std::endl;
+
+            if (mode == "3")
+            {
+                // Цикл для відображення стану гри в режимі AI vs AI
+                while (true)
+                {
+                    response = serial.sendMessage("GetGameState\n");
+
+                    // Виводимо стан дошки
+                    if (response.find("BoardState:") == 0)
+                    {
+                        std::string boardState = response.substr(12);
+                        serial.drawBoard(boardState);
+                    }
+
+                    std::cout << response << std::endl;
+
+                    // Перевіряємо, чи гра завершилася
+                    if (response.find("Wins") != std::string::npos || response.find("Draw") != std::string::npos)
+                    {
+                        std::cout << "The game is over!" << std::endl;
+                        break;
+                    }
+                }
+            }
+
+            if (mode != "3")
+            {
+                while (true)
+                {
+                    std::string input;
+                    std::cout << "Enter your move (1-9) or 'exit' to exit: ";
+                    std::getline(std::cin, input);
+
+                    if (input == "exit")
+                    {
+                        break;
+                    }
+
+                    try
+                    {
+                        int move = std::stoi(input);
+                        if (move < 1 || move > 9)
+                        {
+                            std::cout << "Incorrect entry. Enter a number between 1 and 9." << std::endl;
+                            continue;
+                        }
+
+                        if (mode == "2") {
+                            response = serial.sendMessage("Move " + input + "\n");
+                            std::cout << "Server response: " << response << std::endl;
+                        }
+                        else {
+                            response = serial.sendMessage("Move " + input + "\n");
+                            std::cout << "Server response:\n" << response << std::endl;
+                        }
+
+                        if (response.find("BoardState:") == 0)
+                        {
+                            std::string boardState = response.substr(12);
+                            serial.drawBoard(boardState);
+                        }
+
+                        if (response.find("Wins") != std::string::npos || response.find("Draw") != std::string::npos)
+                        {
+                            std::cout << "The game is over!" << std::endl;
+                            break;
+                        }
+                    }
+                    catch (...)
+                    {
+                        std::cout << "Input recognition error. Enter the correct number." << std::endl;
+                    }
+                }
+
+                serial.disconnect();
+            }
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "An error has occurred: " << e.what() << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown error!" << std::endl;
     }
 
-    // Відправляємо повідомлення на Arduino
-    std::string messageToSend;
-    std::cout << "Input msg:" << std::endl;
-    std::cin >> messageToSend;
-    //std::string messageToSend = "Hello, Arduino!";
-    sendMessage(hSerial, messageToSend);
-
-    // Виводимо повідомлення на консоль
-    std::cout << "Message sent: " << messageToSend << std::endl;
-
-    // Читаємо відповідь від Arduino
-    std::string response = readMessage(hSerial);
-
-    // Виводимо відповідь на консоль
-    std::cout << "Message received: " << response << std::endl;
-
-    // Закриваємо серійний порт
-    CloseHandle(hSerial);
+    system("pause");
     return 0;
 }
